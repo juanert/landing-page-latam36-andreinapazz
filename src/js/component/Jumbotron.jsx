0f3f9e031cd5d076798c489382e5720459679df3
andreinapazz <108377491+andreinapazz@users.noreply.github.com>
2024-12-14 03:22:31 +0000

    hola proyecto

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 export const Jumbotron = () => {
     return(
+        <div className="col-12 col-sm-6 col-lg-6 p-5 d-flex">
         <div className="jumbotron-fluid container-fluid p-5 bg-light rounded-2">
-            <h1 className="display-4">Bienvenidos amantes de Rawayana!</h1>
-            <p className="lead">¿Quién trae las cornetas? New Album</p>
-            <hr className="my-4"/>
-            <p> Originarios de Venezuela, se han convertido en uno de las bandas más emocionantes de 
-                Latinoamérica con su espectáculo en vivo de gran energia y su distintiva fusión de reggae, funk, soul, rock y ritmos caribeños.</p>
-            <a className="btn btn-info  btn-lg" href="#" role="button">Ver Albumes</a>
+            <h1 className="display-4">Bienvenidos amantes de la cultura!</h1>
+            <p className="lead">Cultural Wave, tu plataforma Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, ullam assumenda maxime et aliquid sapiente perspiciatis consequatur magni cumque, dignissimos iusto quo distinctio deserunt itaque odit exercitationem.</p>
+        </div>
+    
         </div>
     );
 };
--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -2,29 +2,46 @@ import React from 'react';
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg text-center bg-dark navbar-dark border-bottom border-body">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">RAWAYANA</a>
+        
+        <nav className="navbar navbar-expand-lg text-center bg-dark navbar-dark border-bottom border-body rounded-2 m-0 p-0">
+            <div className="container-fluid d-flex justify-content-between">
+                <a className="navbar-brand align-items-center" href="#">
+                    <img className="object-cover object-center" src="https://res.cloudinary.com/dj6gqmozm/image/upload/f_auto,q_auto/culturalwavelogo" alt="logo-culturalWave"></img>
+                </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-                <div className="collapse navbar-collapse text-light" id="navbarNav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
                     <ul className="navbar-nav">
-                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                     <li className="nav-item ">
+                        <a className="nav-link" href="#">Conciertos</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Sobre nosotros</a>
+                        <a className="nav-link" href="#">Teatro</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Servicios</a>
+                        <a className="nav-link" href="#">Obras/Museos</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Contacto</a>
+                        <a className="nav-link" href="#">Familia</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Turismo</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Vida Nocturna</a>
                     </li>
                     </ul>
-                </div>
+                    </div>
+                    <div>
+                        <button type="button" class="btn btn-light">
+                            <span href="#" className="log-in">Sing up / Log In</span>
+                        </button>
+                    </div>
             </div>
+                
+                
          </nav>
+         
     );
 };

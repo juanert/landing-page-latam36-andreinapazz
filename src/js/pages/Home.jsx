b78da59f5d665cb711bdd638608a1da820551d68
juanert <61994784+juanert@users.noreply.github.com>
2024-12-20 17:49:41 -0400

    Update Home.jsx

--- src/js/pages/Home.jsx
+++ src/js/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
       <NavBar />
       <h1>Home</h1>
+      <Link to={"/eventos/crear"}>
+        <button>Crear tu evento</button>
+      </Link>
     </div>
   );
 }
